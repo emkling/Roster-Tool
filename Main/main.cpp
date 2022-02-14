49164b9091af517b29b6fe151ae9086f9114ca13
emkling <85776239+emkling@users.noreply.github.com>
2022-02-14 00:00:44 +0000

    added program files

---
+++ Main/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include "roster.h"
+#include "student.h"
+
+using namespace std;
+
+int main()
+{
+    const string studentData[] =
+    { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+      "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+      "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+      "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+      "A5,Eli,Klinh,ekling6@gmail.com,23,25,53,21,SOFTWARE" };
+
+    // Prints User Information
+    cout << "Student Data:" << endl << "\n";
+ 
+    // Creates roster instance
+    Roster classRoster;
+    
+    // Roster classRoster = student;
+    for (int i = 0; i < 5; i++) 
+    {
+        classRoster.parse(studentData[i]);
+    }
+
+     classRoster.printAll();
+     classRoster.printInvalidEmails(); 
+     classRoster.printByDegreeProgram(SOFTWARE);
+
+     for (int i = 0; i < 5; i++)
+    {
+        classRoster.printAverageDaysInCourse(classRoster.classRosterArray[i]->getStudent_ID());
+    }
+     cout << "\n";
+
+     classRoster.remove("A3");
+     classRoster.remove("A3");
+}
---
+++ Main/student.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+#include "degree.h"
+using namespace std;
+
+student::student (string student_id, string firstName, string lastName, string email,
+	int age, int daysToCompletion[],
+	DegreeProgram degreeProgram) {
+
+	this->student_id = student_id;
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->email = email;
+	this->age = age;
+	this->daysToCompletion[0] = daysToCompletion[0];
+	this->daysToCompletion[1] = daysToCompletion[1];
+	this->daysToCompletion[2] = daysToCompletion[2];
+	this->degreeProgram = degreeProgram;
+}
+
+
+
+//Mutator Functions
+
+void student::setDaysToCompletion(int daysToCompletion[]){
+
+	this->daysToCompletion[0] = daysToCompletion[0];
+	this->daysToCompletion[1] = daysToCompletion[1];
+	this->daysToCompletion[2] = daysToCompletion[2];
+}
+
+void student::setAge(int age) {
+
+	this->age = age;
+}
+
+void student::setFirstName(string firstName) {
+	this->firstName = firstName;
+}
+
+void student::setLastName(string lastName) {
+	this->lastName = lastName;
+}
+
+void student::setEmail(string email) {
+	this->email = email;
+}
+
+void student::setDegreeProgram(DegreeProgram degreeProgram) {
+	this->degreeProgram = degreeProgram;
+}
+
+
+//Accessor Functions
+
+string student::getStudent_ID() {
+
+	return student_id;
+
+}
+
+string student::getFirstName() {
+	
+	return firstName;
+
+}
+
+string student::getLastName() {
+
+	return lastName;
+}
+
+string student::getEmail() {
+
+	return email;
+}
+
+int student::getAge() {
+
+	return age;
+}
+
+int* student::getDaysToCompletion() {
+
+	return daysToCompletion;
+}
+
+
+DegreeProgram student::getDegreeInfo() {
+
+	return degreeProgram;
+}
+
+//Print Function
+
+void student::print()
+{
+	cout << getStudent_ID() << "\t";
+	cout << getFirstName() << "\t";
+	cout << getLastName() << "\t";
+	cout << getEmail() << "\t";
+	cout << getAge() << "\t";
+	cout << "{" << getDaysToCompletion()[0] << ",";
+	cout << getDaysToCompletion()[1] << ",";
+	cout << getDaysToCompletion()[2] << "}\t";
+	cout << degreeString[getDegreeInfo()] << endl;
+}

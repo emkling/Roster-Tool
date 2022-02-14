59d9ae333f303503ec36e620726d7b757e83158c
emkling <85776239+emkling@users.noreply.github.com>
2022-02-13 22:32:52 -0500

    Delete main directory

--- main/degree.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEGREE
-#define DEGREE
-#pragma once
-#include <string>
-enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
-const std::string degreeString[] = {"SECURITY", "NETWORK", "SOFTWARE"};
-#endif
